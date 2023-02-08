9ac25d5c034ce49d57f18b763184df53cc5fbb45
Rowanus <r.piek79@gmail.com>
2023-02-08 14:31:54 +0100

    Created service layer, woth student_service.py. And imported student_service.py methods to default_controller.py, which now calls these methods.

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -3,23 +3,19 @@ import six
 
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
+from swagger_server.service.student_service import *
 
-
-def add_student(body=None):  # noqa: E501
+def add_student(body=None): # noqa: E501
     """Add a new student
-
     Adds an item to the system # noqa: E501
-
     :param body: Student item to add
     :type body: dict | bytes
-
     :rtype: float
     """
     if connexion.request.is_json:
-        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
-
-
+        body = Student.from_dict(connexion.request.get_json()) # noqa: E501
+        return add(body)
+    return 500, 'error'
 def delete_student(student_id):  # noqa: E501
     """deletes a student
 
@@ -30,7 +26,7 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    return delete(student_id)
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +39,4 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return get_student_by_id(student_id)
